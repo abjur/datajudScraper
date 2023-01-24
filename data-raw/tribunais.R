e2302d9c971b8f5a7a6297caf554f5dd9a4492a5
jtrecenti <julio.trecenti@gmail.com>
2023-01-24 17:28:40 +0000

    atualizacao de dados dos tribunais

--- data-raw/tribunais.R
+++ data-raw/tribunais.R
@@ -1,10 +1,24 @@
 ## code to prepare `tribunais` dataset goes here
 
+path_municipios <- "data-raw/municipios"
+path_ojs <- "data-raw/ojs"
+fs::dir_create(path_municipios)
+fs::dir_create(path_ojs)
+
+# tribunais ---------------------------------------------------------------
 tribunais <- listar_tribunais()
 
-purrr::walk(tribunais$id, listar_municipios, "data-raw/municipios/")
 
-municipios <- fs::dir_ls("data-raw/datajud/municipios/") |>
+# municipios --------------------------------------------------------------
+
+purrr::walk(
+  tribunais$id,
+  listar_municipios,
+  "data-raw/municipios",
+  .progress = TRUE
+)
+
+municipios <- fs::dir_ls(path_municipios) |>
   purrr::map(jsonlite::read_json, simplifyDataFrame = TRUE) |>
   purrr::map_dfr(tibble::as_tibble, .id = "file") |>
   dplyr::transmute(
@@ -13,20 +27,26 @@ municipios <- fs::dir_ls("data-raw/datajud/municipios/") |>
     nome
   )
 
+# ojs ---------------------------------------------------------------------
+
+# download
 purrr::walk2(
   municipios$id_tribunal,
   municipios$id_municipio,
   listar_oj,
-  "data-raw/datajud/ojs/"
+  path_ojs,
+  .progress = TRUE
 )
 
-ojs <- fs::dir_ls("data-raw/datajud/ojs") |>
+# parse
+ojs <- fs::dir_ls(path_ojs) |>
   purrr::map(jsonlite::read_json, simplifyDataFrame = TRUE) |>
   purrr::map_dfr(tibble::as_tibble, .id = "file") |>
   janitor::clean_names() |>
   dplyr::filter(is.na(timestamp)) |>
   dplyr::select(-timestamp, -status, -error, -path)
 
+# export ------------------------------------------------------------------
 
 usethis::use_data(tribunais, overwrite = TRUE, compress = "xz")
 usethis::use_data(municipios, overwrite = TRUE, compress = "xz")
